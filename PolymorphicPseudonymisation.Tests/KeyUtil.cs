3ad77e247429e16b2b7ef479228a9e54d0459b94
Martijn Kooij <internet@captaincaveman.nl>
2018-09-12 21:42:32 +0200

    Cleaning up tests

--- PolymorphicPseudonymisation.Tests/KeyUtil.cs
+++ PolymorphicPseudonymisation.Tests/KeyUtil.cs
@@ -5,23 +5,37 @@ using PolymorphicPseudonymisation.Key;
 
 namespace PolymorphicPseudonymisation.Tests
 {
-    public class KeyUtil
+    public static class KeyUtilities
     {
-        private IdentityDecryptKey decryptKey;
-        private EncryptedVerifiers verifiers;
-        private EncryptedVerifiers pVerifiers;
-        private PseudonymDecryptKey pDecryptKey;
-        private PseudonymClosingKey pClosingKey;
         private const string IdentityPoint = "AmUppru04ghsI/FvbvV59eoX3lCUWlMAZKu1pPdlvixch5avV+aFwQg=";
         private const string PseudonymPoint = "A9GtKDUn++nl2NWtN4F/2id1gmBhxn4I6Qr9BfeMN+fjNuXGvE79qHc=";
 
-        public KeyUtil()
+        public static IdentityDecryptKey GetIdentityDecryptKey(string identityKeyPem)
         {
-            GetIdentityKeys();
-            GetPseudoKeys();
+            return DecryptKey.FromPem<IdentityDecryptKey>(identityKeyPem);
         }
 
-        private void GetIdentityKeys()
+        public static EncryptedVerifiers GetIdentityVerifiers(IdentityDecryptKey identityDecryptKey)
+        {
+            return identityDecryptKey.ToVerifiers(IdentityPoint);
+        }
+
+        public static PseudonymDecryptKey GetPseudonymDecryptKey(string pseudoKeyPem)
+        {
+            return DecryptKey.FromPem<PseudonymDecryptKey>(pseudoKeyPem);
+        }
+
+        public static PseudonymClosingKey GetPseudonymClosingKey(string pseudoClosingKeyPem)
+        {
+            return DecryptKey.FromPem<PseudonymClosingKey>(pseudoClosingKeyPem);
+        }
+
+        public static EncryptedVerifiers GetPseudonymVerifiers(PseudonymDecryptKey pseudonymDecryptKey)
+        {
+            return pseudonymDecryptKey.ToVerifiers(PseudonymPoint);
+        }
+
+        private static void GetIdentityKeys()
         {
             // Convert P7 key to PEM
             /*using (Stream file = new FileStream("resources\\" + "p7\\ID-4.p7", FileMode.Open, FileAccess.Read))
@@ -33,15 +47,9 @@ namespace PolymorphicPseudonymisation.Tests
                 verifiers = decryptKey.ToVerifiers(IdentityPoint);
             }*/
 
-            var identityKeyPem = File.ReadAllText("resources\\keys\\id-4.pem");
-            // Convert PEM to IdentityDecryptKey
-            decryptKey = Key.DecryptKey.FromPem<IdentityDecryptKey>(identityKeyPem);
-            // Derive verifier (for signature verifying) from key
-            verifiers = decryptKey.ToVerifiers(IdentityPoint);
-
         }
 
-        private void GetPseudoKeys()
+        private static void GetPseudoKeys()
         {
             /*using (Stream file = new FileStream("resources\\" + "p7\\PD-4.p7", FileMode.Open, FileAccess.Read))
             {
@@ -52,22 +60,12 @@ namespace PolymorphicPseudonymisation.Tests
                 pVerifiers = pDecryptKey.ToVerifiers(PseudonymPoint);
             }*/
 
-            var pseudoKeyPem = File.ReadAllText("resources\\keys\\pd-4.pem");
-            // Convert PEM to IdentityDecryptKey
-            pDecryptKey = Key.DecryptKey.FromPem<PseudonymDecryptKey>(pseudoKeyPem);
-            // Derive verifier (for signature verifying) from key
-            pVerifiers = pDecryptKey.ToVerifiers(PseudonymPoint);
-
             /*using (Stream file = new FileStream("resources\\" + "p7\\PC-4.p7", FileMode.Open, FileAccess.Read))
             {
                 var pseudoClosingKeyPem = Cms.Read(PrivateKey, file);
                 // Convert PEM to IdentityDecryptKey
                 pClosingKey = Key.DecryptKey.FromPem<PseudonymClosingKey>(pseudoClosingKeyPem);
             }*/
-            var pseudoClosingKeyPem = File.ReadAllText("resources\\keys\\pc-4.pem");
-
-            // Convert PEM to IdentityDecryptKey
-            pClosingKey = Key.DecryptKey.FromPem<PseudonymClosingKey>(pseudoClosingKeyPem);
         }
 
         private static AsymmetricKeyParameter PrivateKey
@@ -82,15 +80,5 @@ namespace PolymorphicPseudonymisation.Tests
                 return privateKey;
             }
         }
-
-        public virtual IdentityDecryptKey DecryptKey => decryptKey;
-
-        public virtual EncryptedVerifiers Verifiers => verifiers;
-
-        public virtual EncryptedVerifiers PVerifiers => pVerifiers;
-
-        public virtual PseudonymDecryptKey PDecryptKey => pDecryptKey;
-
-        public virtual PseudonymClosingKey PClosingKey => pClosingKey;
     }
 }
--- PolymorphicPseudonymisation.Tests/Utilities/DecryptUtilTests.cs
+++ PolymorphicPseudonymisation.Tests/Utilities/DecryptUtilTests.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PolymorphicPseudonymisation.Utilities;
 
@@ -10,18 +9,33 @@ namespace PolymorphicPseudonymisation.Tests.Utilities
     public class DecryptUtilTests
     {
         [TestMethod]
-        public void Decrypt()
+        public void GetIdentity()
         {
-            var keys = new KeyUtil();
+            var encryptedIdentity = File.ReadAllText("resources\\" + "signed\\950053533-3-4-I.txt");
 
-            var ei = File.ReadAllText("resources\\" + "signed\\950053533-3-4-I.txt");
-            var ep = File.ReadAllText("resources\\" + "signed\\950053533-3-4-P.txt");
+            var identityKeyPem = File.ReadAllText("resources\\keys\\id-4.pem");
+            var identityDecryptKey = KeyUtilities.GetIdentityDecryptKey(identityKeyPem);
+            var encryptedVerifiers = KeyUtilities.GetIdentityVerifiers(identityDecryptKey);
 
-            var simBsn = DecryptUtil.GetIdentity(ei, keys.DecryptKey, keys.Verifiers);
-            var simPseudo = DecryptUtil.GetPseudonym(ep, keys.PDecryptKey, keys.PClosingKey, keys.PVerifiers);
+            var actualBsn = DecryptUtil.GetIdentity(encryptedIdentity, identityDecryptKey, encryptedVerifiers);
 
-            Assert.AreEqual("950053533", simBsn);
-            Assert.AreEqual("0000004404CAC3926533F301A13500D2379D383AD8717D1585F4174473AC0A715FE4786BC0A41B1D872BABBBB8C917945E5006FBDF61BFFAEC478979C72163FAF56A645496C3038C6A1F13E0B623384DD031B16F30", simPseudo);
+            Assert.AreEqual("950053533", actualBsn);
+        }
+
+        [TestMethod]
+        public void GetPseudonym()
+        {
+            var encryptedPseudonym = File.ReadAllText("resources\\" + "signed\\950053533-3-4-P.txt");
+            var pseudoKeyPem = File.ReadAllText("resources\\keys\\pd-4.pem");
+            var pseudoClosingKeyPem = File.ReadAllText("resources\\keys\\pc-4.pem");
+
+            var pseudonymClosingKey = KeyUtilities.GetPseudonymClosingKey(pseudoClosingKeyPem);
+            var pseudonymDecryptKey = KeyUtilities.GetPseudonymDecryptKey(pseudoKeyPem);
+            var pseudonymVerifiers = KeyUtilities.GetPseudonymVerifiers(pseudonymDecryptKey);
+
+            var actualPseudonym = DecryptUtil.GetPseudonym(encryptedPseudonym, pseudonymDecryptKey, pseudonymClosingKey, pseudonymVerifiers);
+
+            Assert.AreEqual("0000004404CAC3926533F301A13500D2379D383AD8717D1585F4174473AC0A715FE4786BC0A41B1D872BABBBB8C917945E5006FBDF61BFFAEC478979C72163FAF56A645496C3038C6A1F13E0B623384DD031B16F30", actualPseudonym);
         }
     }
 }
